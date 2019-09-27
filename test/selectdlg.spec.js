391d9a95058d93cd29c97cd1a0096909bbf2f229
mskang <minsgiman@gmail.com>
2019-09-27 18:13:01 +0900

    react enzyme 테스트 적용

--- test/selectdlg.spec.js
+++ test/selectdlg.spec.js
@@ -1,43 +1,45 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {act} from 'react-dom/test-utils';
+import { mount } from 'enzyme';
 import SelectDlg from './../src/components/selectDlg';
+import jest from 'jest-mock';
 
-let container;
-
-beforeEach(() => {
-    container = document.createElement('div');
-    document.body.appendChild(container);
-});
-
-afterEach(() => {
-    document.body.removeChild(container);
-    container = null;
-});
-
-it('select dialog test', () => {
+describe('SelectDlg', () => {
     const title = 'TestTitle';
     const message = 'TestMessage';
     const okValue = 'ok';
     const nokValue = 'nok';
+    const selectFn = jest.fn();
+    let component = null;
 
-    act(() => {
-        ReactDOM.render(<SelectDlg onDlgClose={()=>alert('close')}
-                title={title}
-                message={message}
-                onSelect={selectCb}
-                firstvalue={okValue}
-                secondvalue={nokValue}
-        />, container);
+    beforeEach(() => {
+        component = mount(<SelectDlg onDlgClose={()=>alert('close')}
+                                     title={title}
+                                     message={message}
+                                     onSelect={selectFn}
+                                     firstvalue={okValue}
+                                     secondvalue={nokValue}/>);
     });
-    expect(container.textContent).toContain(title);
-    expect(container.textContent).toContain(message);
 
-    const firstBtn = container.querySelector('button');
-    act(() => {
-        firstBtn.dispatchEvent(new MouseEvent('click', {bubbles: true}));
+    afterEach(() => {
+        component.unmount();
     });
-    function selectCb(value) {
-        expect(value).toBe(okValue);
-    }
+
+
+    it('match snapshot', () => {
+        expect(component).toMatchSnapshot();
+    });
+
+    it('renders title and message', () => {
+        expect(component.props().title).toBe(title);
+        expect(component.props().message).toBe(message);
+        expect(component.find('h2').contains(title)).toBe(true);
+        expect(component.find('p').text()).toBe(message);
+    });
+
+    it('click select button', () => {
+        const firstBtn = component.find('button:first-child');
+        firstBtn.simulate('click');
+        expect(selectFn).toBeCalledWith(okValue);
+    });
 });
+
